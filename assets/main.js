9ceb04752c0047badd3b563aa6f5982e8f3b9078
dwu <dwu@catchpoint.com>
2019-03-22 16:26:12 -0400

    Initial Commit

--- assets/main.js
+++ assets/main.js
@@ -1,84 +1,114 @@
 $(function() {
    var client = ZAFClient.init();
-   client.invoke('resize', { width: '100%', height: '125px' });
-/* Things to do:
-
-1. Make Hot Client appear only if it is Hot Client, otherwise, nothing.
-2. Have 2 buttons, Add To Pre-Sale Team, Add to Post-Sale Team.
-3. If you click on any button, have a message, these people have been added.  And also automatically add. 
-
-*/
-
-  /* 
-   let options = {
-     url: '/api/v2/tickets.json',
-     type: 'GET'
-   };
-    
-   return client.request(options).then((results) => {
-     console.log("Example 1 call results:", results);
-   });
-   */
-  //element.insertAdjacentText(position, element)
-
-   document.getElementById("csm").insertAdjacentText('afterbegin', 'No CSM assigned')
-   document.getElementById("cse").insertAdjacentText('afterbegin', 'No CSE assigned')
-
+   client.invoke('resize', { width: '100%', height: '120px' });
+   
+   document.getElementById("csm").insertAdjacentText('afterbegin', 'No CSM assigned');
+   document.getElementById("prePE").insertAdjacentText('afterbegin', 'No pre PE assigned');
+   document.getElementById("postPE").insertAdjacentText('afterbegin', 'No CSE assigned');
+   document.getElementById("ae").insertAdjacentText('afterbegin', 'No AE assigned');
+  
   client.get('ticket.requester').then((results) => {
-    //console.log("results.ticket.requester:", results["ticket.requester"]["organizations"][0]["organizationFields"]["hot_client"])  
-    let hot_client_flag = results["ticket.requester"]["organizations"][0]["organizationFields"]["hot_client"];
-    
-    if(hot_client_flag){
-        //document.getElementById("hot_client").checked="checked";
-    } else {
-        //document.getElementById("hot_client").checked="";
-    }
+    let ping = results["ticket.requester"]["organizations"][0]["organizationFields"]["ping"]; 
+    let hotClient = results["ticket.requester"]["organizations"][0]["organizationFields"]["hot_client"];
+        
+    let pingNotification = document.createElement("div");
+    let hotClientNotification = document.createElement("div");
 
-    //console.log("results.ticket.CSM:", results["ticket.requester"]["organizations"][0]["organizationFields"]);
+    pingNotification.innerHTML = `${ping ? ping : '&nbsp'}`;
+    hotClientNotification.innerHTML = `${hotClient ? 'Hot Client' :  '&nbsp'}`;
+    
+    Object.assign(pingNotification.style,{display: "inline-block", border: "1px solid black", margin: "2px", padding: "3px", width: "120px", color: "white", fontSize: "12px" });
+    Object.assign(hotClientNotification.style,{display: "inline-block", border: "1px black", margin: "2px", padding: "3px", width: "120px", color: "white", fontSize: "12px" });
+                
+      switch (ping) {
+        case 'Protect': Object.assign(pingNotification.style,{color: "white", background: "red" });
+        break;
+        case 'Invest': Object.assign(pingNotification.style,{color: "black", background: "yellow" });
+        break;
+        case 'Nurture': Object.assign(pingNotification.style,{color: "black", background: "yellow" });
+        break;
+        case 'Grow': Object.assign(pingNotification.style,{color: "black", background: "LawnGreen" });
+        break;
+        default : Object.assign(pingNotification.style,{borderStyle: "none", color: "white", background: "white" });
+        break;
+      }
+
+      switch (hotClient) {
+        case true: Object.assign(hotClientNotification.style,{borderStyle: "solid", color: "white", background: "red" });
+        break;
+        case false: Object.assign(hotClientNotification.style,{borderStyle: "none", color: "black", background: "white" });
+        break;
+        default : Object.assign(hotClientNotification.style,{color: "white", background: "red" });
+        break;
+      }
+   
+    document.getElementById("pingStatus").insertAdjacentElement('afterbegin', pingNotification);
+    document.getElementById("hotClientStatus").insertAdjacentElement('afterbegin', hotClientNotification);
+    
     let customer_success_manager_name = results["ticket.requester"]["organizations"][0]["organizationFields"]["customer_success_manager"];
     let customer_success_manager_email = results["ticket.requester"]["organizations"][0]["organizationFields"]["csm_email"];
 
-    if (customer_success_manager_name) {
-        //document.getElementById("csm").textContent=`CSM: ${customer_success_manager_name}`
-        let csmButton = document.createElement("button");
+    if (customer_success_manager_name) { 
+      let csmButton = document.createElement("button");
         csmButton.onclick = function () {
           client.invoke('ticket.collaborators.add', { email: customer_success_manager_email} );
         };
-        csmButton.setAttribute("style", "padding: 3px");
-        csmButton.innerHTML = ` Add ${customer_success_manager_name} `;
+        
+        Object.assign(csmButton.style,{padding: "1px", margin: "1px", fontSize: "12px"});
+        csmButton.innerHTML = ` Copy ${customer_success_manager_name} `;
         document.getElementById("csm").innerHTML = "";
         document.getElementById("csm").insertAdjacentElement('afterbegin', csmButton);
     }
 
-    //console.log("results.ticket.requester:", results["ticket.requester"]["organizations"][0]["organizationFields"])
-    let customer_success_engineer_name = results["ticket.requester"]["organizations"][0]["organizationFields"]["post_pe"];
-    let customer_success_engineer_email = results["ticket.requester"]["organizations"][0]["organizationFields"]["post_pe_email"];
+    let post_performance_engineer_name = results["ticket.requester"]["organizations"][0]["organizationFields"]["post_pe"];
+    let post_performance_engineer_email = results["ticket.requester"]["organizations"][0]["organizationFields"]["post_pe_email"];
 
-    if (customer_success_manager_name) {
-        //document.getElementById("cse").textContent=`CSE: ${customer_success_engineer_name}`
-
-        let cseButton = document.createElement("button");
-        cseButton.onclick = function () {
-          client.invoke('ticket.collaborators.add', { email: customer_success_engineer_email} );
+    if (post_performance_engineer_name) { 
+      let postPeButton = document.createElement("button");
+        postPeButton.onclick = function () {
+          client.invoke('ticket.collaborators.add', { email: post_performance_engineer_email } );
         };
-        cseButton.setAttribute("style", "padding: 3px");
-        cseButton.innerHTML = ` Add ${customer_success_engineer_name} `;
-        document.getElementById("cse").innerHTML = "";
-        document.getElementById("cse").insertAdjacentElement('afterbegin', cseButton);
+        
+        Object.assign(postPeButton.style,{padding: "1px", margin: "1px", fontSize: "12px"});
+        postPeButton.innerHTML = ` Copy ${post_performance_engineer_name} `;
+        document.getElementById("postPE").innerHTML = "";
+        document.getElementById("postPE").insertAdjacentElement('afterbegin', postPeButton);
     }
+ 
+    let pre_performance_engineer_name = results["ticket.requester"]["organizations"][0]["organizationFields"]["pre_pe"];
+    let pre_performance_engineer_email = results["ticket.requester"]["organizations"][0]["organizationFields"]["pre_pe_email"];
+
+    if (pre_performance_engineer_name) {
+
+        let prePeButton = document.createElement("button");
+        prePeButton.onclick = function () {
+          client.invoke('ticket.collaborators.add', { email: pre_performance_engineer_email} );
+        };
+        
+        Object.assign(prePeButton.style,{padding: "1px", margin: "1px", fontSize: "12px"});
+        prePeButton.innerHTML = ` Copy ${pre_performance_engineer_name} `;
+        document.getElementById("prePE").innerHTML = "";
+        document.getElementById("prePE").insertAdjacentElement('afterbegin', prePeButton);
+    }
+
+    let account_executive_name = results["ticket.requester"]["organizations"][0]["organizationFields"]["ae"];
+    let account_executive_email = results["ticket.requester"]["organizations"][0]["organizationFields"]["ae_email"];
+
+    if (account_executive_name) {
+        let aeButton = document.createElement("button");
+        aeButton.onclick = function () {
+          client.invoke('ticket.collaborators.add', { email: account_executive_email} );
+        };
+        
+        Object.assign(aeButton.style,{padding: "1px", margin: "1px", fontSize: "12px"});
+        aeButton.innerHTML = ` Copy ${account_executive_name} `;
+        document.getElementById("ae").innerHTML = "";
+        document.getElementById("ae").insertAdjacentElement('afterbegin', aeButton);
+    }
+
 
   }).catch((error) => {
-      document.getElementById("hot_client").textContent=`Error retrieving hot client: ${error}`
+    document.getElementById("error").textContent=`Error retrieving ping information from ZD: ${error}`
   });
 
-/*
-  client.get('ticket.requester').then((results) =>{
-    console.log("results.ticket.CSM:", results["ticket.requester"]["organizations"][0]["organizationFields"]["customer_success_manager"])
-    let customer_success_manager_name = results["ticket.requester"]["organizations"][0]["organizationFields"]["customer_success_manager"];
-
-    if (customer_success_manager_name) {
-        document.getElementById("csm").textContent=`CSM: ${customer_success_manager_name}`
-    }
-  })
-  */
 });
